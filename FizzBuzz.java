914d24577dd4a5b8a5a93717505789f91c63ce89
kkthxby3 <shiffer@usc.edu>
2013-10-30 16:44:34 -0700

    Aded rule for Fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,6 +1,10 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i < 100; i++) 
+        {
+        	if (i%3==0)
+        		for (int i=0; i<2; i++)
+        			System.out.println(i);
             System.out.println(i);
         }
     }
